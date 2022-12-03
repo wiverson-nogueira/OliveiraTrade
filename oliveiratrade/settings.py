37b9d584828981fda6969c95808c29b108a3e908
Wiverson Lima <68648223+wiverson-nogueira@users.noreply.github.com>
2022-12-03 15:15:55 -0300

    Update settings.py

--- oliveiratrade/settings.py
+++ oliveiratrade/settings.py
@@ -80,13 +80,6 @@ WSGI_APPLICATION = 'oliveiratrade.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-DATABASES = {
-    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'oliveiratrade', 
         'USER': 'oliveiratrade_user',
